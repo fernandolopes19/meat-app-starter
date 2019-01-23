61c8f45800f92d847d1de13d8824a4d4c051f235
Fernando Souza Lopes <fsouza.lopes@bbtecno.com.br>
2019-01-23 17:40:59 -0200

    primeiro commit

---
+++ src/app/order/order-item/order-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+
+@Component({
+  selector: "mt-order-item",
+  templateUrl: "./order-item.component.html"
+})
+export class OrderItemComponent implements OnInit {
+  @Input() items: CartItem[];
+
+  @Output() increseQty = new EventEmitter<CartItem>();
+  @Output() decreaseQty = new EventEmitter<CartItem>();
+  @Output() remove = new EventEmitter<CartItem>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  emitIncreaseQty(item: CartItem) {
+    this.increseQty.emit(item);
+  }
+
+  emitDecreaseQty(item: CartItem) {
+    this.decreaseQty.emit(item);
+  }
+
+  emitRemove(item: CartItem) {
+    this.remove.emit(item);
+  }
+}
